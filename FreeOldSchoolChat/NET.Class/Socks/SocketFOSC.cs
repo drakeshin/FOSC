f7b95f5fe7ea3bf17b2fa422c936b89769f61a9e
luiz.neto <luiz.neto@ID-WK-0146>
2017-08-17 14:04:10 -0300

    developing the net class

---
+++ FreeOldSchoolChat/NET.Class/Socks/SocketFOSC.cs
@@ -0,0 +1,40 @@
+﻿using System.Net;
+using System.Net.Sockets;
+
+namespace NET.Class.Socks
+{
+    class SocketFOSC
+    {
+        #region Vars
+        private Socket _Socket;
+        private NetworkStream _NetStream;
+        private IPEndPoint _IPEP;
+        private EndPoint _EP;
+        private int _PORT = 0;
+        #endregion
+
+        #region Public Methods
+        public SocketFOSC(Socket usedSck, int usedPort)
+        {
+            _PORT = usedPort;
+            try
+            {
+                _Socket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+                _IPEP = new IPEndPoint(IPAddress.Any, _PORT);
+                _Socket.Bind(_IPEP);
+                _EP = (EndPoint)_IPEP;
+
+
+            }
+            catch(SocketException scke)
+            {
+                throw scke;
+            }
+            finally
+            {
+                _PORT = 0;
+            }
+        }
+        #endregion
+    }
+}
